1e0a26ee7419f6fa9210d69c19ba65ad38cbc1d2
kacpermajcher <kacpermajcher13@gmail.com>
2024-01-26 14:15:52 +0100

    Create slider

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,8 +1,15 @@
 import 'package:audio_players/features/home/widgets/app_bar.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isPlaying = false;
 
   @override
   Widget build(BuildContext context) {
@@ -11,14 +18,81 @@ class HomePage extends StatelessWidget {
       appBar: const CustomAppBar(),
       body: SizedBox(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
               child: Container(
                 height: 450,
                 color: Colors.pink,
               ),
+            ),
+            const SizedBox(height: 20),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Middle of the night',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Elley Duhe',
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                    ],
+                  ),
+                  Icon(
+                    Icons.favorite,
+                    color: Colors.red,
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Slider(
+                value: 0.5,
+                min: 0,
+                onChanged: (value) async {},
+                activeColor: Colors.white,
+                inactiveColor: Colors.white.withOpacity(0.3),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                IconButton(
+                  icon: const Icon(
+                    Icons.skip_previous_rounded,
+                    size: 40,
+                  ),
+                  color: Colors.white,
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(
+                    isPlaying ? Icons.pause : Icons.play_circle,
+                    size: 50,
+                  ),
+                  color: Colors.white,
+                  onPressed: () async {},
+                ),
+                IconButton(
+                  icon: const Icon(
+                    Icons.skip_next_rounded,
+                    size: 40,
+                  ),
+                  color: Colors.white,
+                  onPressed: () {},
+                ),
+              ],
             ),
           ],
         ),
