31939bcc00ea48784968a26b9635260b37961618
kacpermajcher <kacpermajcher13@gmail.com>
2024-01-26 13:34:08 +0100

    Create AppBar

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:audio_players/features/home/widgets/app_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,6 +6,10 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: const CustomAppBar(),
+      body: Container(),
+    );
   }
 }
---
+++ lib/features/home/widgets/app_bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      automaticallyImplyLeading: true,
+      backgroundColor: Colors.black,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.arrow_back_ios_new),
+            color: Colors.white,
+          ),
+          const Text(
+            'Top Songs',
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.menu_rounded),
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
+}
