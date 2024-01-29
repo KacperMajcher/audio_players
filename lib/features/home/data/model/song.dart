a04e629713c4bf4615d2259eb66b54eb3afd1421
kacpermajcher <kacpermajcher13@gmail.com>
2024-01-29 15:56:24 +0100

    Create Song model

---
+++ lib/features/home/data/model/song.dart
@@ -0,0 +1,12 @@
+class Song {
+  Song({
+    required this.songName,
+    required this.artistName,
+    required this.albumImagePath,
+    required this.audioPath,
+  });
+  final String songName;
+  final String artistName;
+  final String albumImagePath;
+  final String audioPath;
+}
